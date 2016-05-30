086e516ce81ab46a840f464d7f6044d946a0c033
Erik Aulin <erik@aulin.co>
2016-05-30 22:39:31 +0300

    fix to tail issue

--- scripts/start-master.sh
+++ scripts/start-master.sh
@@ -2,4 +2,4 @@
 export SPARK_MASTER_IP=`awk 'NR==1 {print $1}' /etc/hosts`
 export SPARK_LOCAL_IP=`awk 'NR==1 {print $1}' /etc/hosts`
 /opt/spark/sbin/start-master.sh --properties-file /spark-defaults.conf -i $SPARK_LOCAL_IP "$@"
-tail -f /var/log/syslog
+tail -f /var/log/faillog
